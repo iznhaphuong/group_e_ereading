44617271dea1603c913887f52f2ea48156613d0d
LinhTrang <20211tt0755@mail.tdc.edu.vn>
2022-06-12 20:09:43 +0700

    create database ratings

---
+++ database/migrations/2022_06_12_092647_create_ratings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->integer('creation_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('creation_id')->references('id')->on('categories');
+            $table->float("star",8,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
