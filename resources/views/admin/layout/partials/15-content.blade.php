d05a008de42e12de2e70f648c6eb4c3c8086e32f
0965655783 <ducnm.90.student@fit.tdc.edu.vn>
2022-06-16 15:24:18 +0700

    Update module 15

--- resources/views/admin/layout/partials/15-content.blade.php
+++ resources/views/admin/layout/partials/15-content.blade.php
@@ -1,37 +1,32 @@
 <div class="type-15">
-    <div id="viewport">
-        <!-- Sidebar -->
-        <div id="sidebar">
-            <header>
-                <a href="#">Ereading</a>
-            </header>
-            <ul class="nav d-flex flex-column">
-                <li class="my-2">
-                    <a href="#">
-                        <i class="fa-solid fa-gauge"></i> Dashboard
-                    </a>
-                </li>
-                <li class="my-2">
-                    <a href="#">
-                        <i class="fa-solid fa-book"></i> Shortcuts
-                    </a>
-                </li>
-                <li class="my-2">
-                    <a href="#">
-                        <i class="fa-solid fa-tags"></i> Overview
-                    </a>
-                </li>
-                <li class="my-2">
-                    <a href="#">
-                        <i class="fa-solid fa-tags"></i> About
-                    </a>
-                </li>
-                <li class="my-2">
-                    <a href="#">
-                        <i class="fa-solid fa-tags"></i> Contact
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
+  <!-- partial:index.partial.html -->
+<div id="viewport">
+  <!-- Sidebar -->
+  <div id="sidebar">
+    <header>
+      <a href="#">Ereading</a>
+    </header>
+    <ul class="nav flex-column mb-3">
+      <li>
+        <a href="#">
+          <div><i class="fa-solid fa-book-bookmark"></i></div> Quản lý truyện
+        </a>
+      </li>
+      <li>
+        <a href="#" >
+          <div>
+          <i class="fa-solid fa-book-open-reader"></i>
+          </div>
+           Quản lý chương
+        </a>
+      </li>
+      <li>
+        <a href="#" >
+          <div><i class="fa-solid fa-users"></i></div>
+         Quản lý người dùng
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
 </div>
