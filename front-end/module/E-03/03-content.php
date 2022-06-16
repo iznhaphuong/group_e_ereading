c4e2aa7f7f688bf839e47e032908f8067f8bf138
ThanhTam <20211tt4665@mail.tdc.edu.vn>
2022-06-16 14:51:40 +0700

    Update module 3, 4, 20

--- front-end/module/E-03/03-content.php
+++ front-end/module/E-03/03-content.php
@@ -5,26 +5,43 @@
             <h4 class="new__stories__title"> Truyện được đề cử</h4>
         </div>
         <div class="image-wrapper">
-            <div class="col-xs-3">
-                <img class="image" src="./images/bat-kham.jpg" alt="">
-                <a href="#" class="title">Bất kham</a>
+            <div class="col-3">
+                <div class="row" style="float: left;">
+                    <div class="col-6" style="padding-bottom: 20px;">
+                        <img class="image-fluid" src="./images/bi-bat-ve-sau-nay-sinh-con-cho-han.jpg" alt="">
+                        <a href="#" class="title" >Bị bắt về sinh con cho hắn</a>
+                        <div class="card-text"><span class="text-muted" style="font-size:12px;">By <a href="#" style="color:black;">Jane Doe</a></span></div>
+                    </div>
+                    <div class="col-6">
+                        <img class="image-fluid" src="./images/bi-bat-ve-sau-nay-sinh-con-cho-han.jpg" alt="">
+                        <a href="#" class="title" >Bị bắt về sinh con cho hắn</a>
+                        <div class="card-text"><span class="text-muted" style="font-size:12px;">By <a href="#" style="color:black;">Jane Doe</a></span></div>
+                    </div>
+                </div>
             </div>
-            <div class="col-xs-3">
-                <img class="image" src="./images/bi-bat-ve-sau-nay-sinh-con-cho-han.jpg" alt="">
-                <a href="#" class="title">Bị bắt</a>
+            <div class="col-6 middle">
+                <img class="image-fluid" src="./images/13-banner-truyen-tranh-online-16064944282141952425197.jpg" alt="">
+                <a href="#" class="title" style="padding-top:15px;">Nông Môn Trưởng Tỷ Có Không Gian</a>
+                <div class="card-text" style="padding-top:10px;"><span class="text-muted" style="font-size:12px;">By <a href="#" style="color:black;">Jane Doe</a></span></div>
+                <div class="content" style="padding-top:10px;">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, labore officiis aspernatur omnis aut.
+                </div>
             </div>
-            <div class="col-xs-3">
-                <img class="image" src="./images/nong-mon-khoa-cu-chi-kiem-tien-khao-thi-duong-gia.jpg" alt="">
-                <a href="#" class="title">Nông môn</a>
-            </div>
-            <div class="col-xs-3">
-                <img class="image" src="./images/nong-mon-khoa-cu-chi-kiem-tien-khao-thi-duong-gia.jpg" alt="">
-                <a href="#" class="title">Nông môn</a>
-            </div>
-            <div class="col-xs-3">
-                <img class="image" src="./images/bat-kham.jpg" alt="">
-                <a href="#" class="title">Bất kham</a>
+            <div class="col-3">
+                <div class="row" style="float: right;">
+                    <div class="col-6" style="padding-bottom: 20px;">
+                        <img class="image-fluid" src="./images/bat-kham.jpg" alt="">
+                        <a href="#" class="title">Bất kham</a>
+                        <div class="card-text"><span class="text-muted" style="font-size:12px;">By <a href="#" style="color:black;">Jane Doe</a></span></div>
+                    </div>
+                    <div class="col-6">
+                        <img class="image-fluid" src="./images/bat-kham.jpg" alt="">
+                        <a href="#" class="title">Bất kham</a>
+                        <div class="card-text"><span class="text-muted" style="font-size:12px;">By <a href="#" style="color:black;">Jane Doe</a></span></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+</div>
 </div>
